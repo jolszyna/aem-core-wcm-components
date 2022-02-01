6f78643e8cc3b2ca3e40ebe66c9d695b9f6dbd3d
Jakub Olszyna <jolszyna@gmail.com>
2022-02-01 18:51:26 +0100

    moved ThemeSelctor model to models/v3

--- bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v3/ThemeSelectorImpl.java
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v3/ThemeSelectorImpl.java
@@ -13,7 +13,7 @@
  ~ See the License for the specific language governing permissions and
  ~ limitations under the License.
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-package com.adobe.cq.wcm.core.components.internal.models.v1;
+package com.adobe.cq.wcm.core.components.internal.models.v3;
 
 import com.adobe.cq.export.json.ComponentExporter;
 import com.adobe.cq.wcm.core.components.models.ThemeSelector;
